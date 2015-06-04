6d09f144d4c37d6f17d462518627dd52370aee08
Perry Gee <pgee@physics.ucdavis.edu>
2015-06-03 18:05:08 -0700

    Changes to allow constants.py entries to be changed from runner scripts
    Note that the subfield_max parameter must be set on the command line
    Also made changes to the numbering of Psfs in the draw_psf_src location

--- great3sims/tests/real_galaxy.py
+++ great3sims/tests/real_galaxy.py
@@ -1,2 +1,12 @@
 import great3sims
-great3sims.run(".", gal_dir="../../../COSMOS_23.5_training_sample", truth_dir="truth", steps=["metaparameters", "catalogs", "config",], experiments=["real_galaxy"], obs_type="ground", shear_type=["constant"], draw_psf_src='sample_psfs/psfs.fits')
+great3sims.constants.image_size_deg = .50
+great3sims.constants.nrows = 10
+great3sims.constants.ncols = 10
+great3sims.constants.n_subfields = 1
+great3sims.constants.xsize["ground"][True] = 64
+great3sims.constants.ysize = great3sims.constants.xsize
+great3sims.constants.n_subfields_per_field["constant"][True] = 1
+great3sims.constants.subfield_grid_subsampling = 1
+great3sims.constants.n_deep_subfields = 0
+great3sims.constants.deep_frac = 0.0
+great3sims.run(".", gal_dir="../../../COSMOS_23.5_training_sample", truth_dir="truth", steps=["metaparameters", "catalogs", "config",], experiments=["real_galaxy"], obs_type="ground", shear_type=["constant"], draw_psf_src='/sandbox/lsstshared/pgee/mylsst9/galaxy_shear_experiments/examples', subfield_max=0)
